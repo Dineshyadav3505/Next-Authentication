745879dc81048449910dbd016b8323c061d5a492
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-16 17:38:57 +0530

    make connection to the server

---
+++ src/dbconfig/dbconfig.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function dbconnect():  Promise<void>{
+    try {
+        // Connect to MongoDB
+        mongoose.connect(process.env.MONGO_URI || "")
+        const connection = mongoose.connection;
+        
+        connection.on('connected', () => {
+            console.log("Connected to MongoDB");
+        });
+
+        connection.on('error', (error) => {
+            console.error("Error connecting to MongoDB:", error);
+            process.exit(1);
+        });
+
+    } catch (error) {
+        console.log("Error while connecting to MongoDB", error);
+    }
+}

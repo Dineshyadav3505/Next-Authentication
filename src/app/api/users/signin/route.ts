95b00405c3e83dc2701d14eb00b101c8d1a48d5f
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-16 23:47:53 +0530

    Remove unused import and add console.log for MONGO_URI

--- src/app/api/users/signin/route.ts
+++
@@ -1,1 +0,0 @@
-import { dbconnect } from "@/dbconfig/dbconfig"
--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 export async function dbconnect():  Promise<void>{
     try {
         // Connect to MongoDB
-        mongoose.connect(process.env.MONGO_URI || "")
+        console.log(process.env.MONGO_URI)
+        mongoose.connect(process.env.MONGO_URI!)
         const connection = mongoose.connection;
         
         connection.on('connected', () => {
